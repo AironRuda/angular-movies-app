5561b76fb771397fee9979a3dfe7bfb541624e37
AironRuda <airon.ruda@udea.edu.co>
2023-03-28 18:10:19 -0500

    feat: movies, movie-list & movie-detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MoviesListComponent } from './components/movies/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    MoviesComponent,
+    MoviesListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
