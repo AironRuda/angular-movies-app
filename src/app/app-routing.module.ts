bca4beb20b391d1d3f18204f6822a54d7d64392b
AironRuda <airon.ruda@udea.edu.co>
2023-03-29 20:37:43 -0500

    feat: movies list template fix: routing correction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MoviesListComponent } from './components/movies/movies-list/movies-list.component';
 
 const routes: Routes = [
   {
-    path: "",
-    pathMatch: "full",
-    redirectTo: "home"
-  }, {
     path: "home",
     component: HomeComponent
+  }, {
+    path: "movies",
+    component: MoviesListComponent
+  }, {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "home"
   }
 ];
 
